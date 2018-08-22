5cf2528b7d6254c8e25737472f5d4ce4fa5d9471
sophia1215 <code.sophiawang@gmail.com>
2018-08-22 15:45:50 +0800

    Install django-cruds-adminlte && Use email as username for Django authentication && new a todo app

--- djcruds_adminlte/settings.py
+++ djcruds_adminlte/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'todo.User'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+
